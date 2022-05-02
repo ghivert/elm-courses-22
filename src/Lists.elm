cd65ce0e86bbc03eb14f98e391e1416839cb08e6
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-05-02 11:24:10 +0200

    ✅ Add some more tests

---
+++ src/Lists.elm
@@ -0,0 +1,25 @@
+module Lists exposing (..)
+
+-- Change function body.
+length : List a -> Int
+length list = 0
+
+-- Change function body.
+member : a -> List a -> Bool
+member mem list = False
+
+-- Change function body.
+filterEven : List Int -> List Int
+filterEven list = []
+
+-- Change function body.
+filter : (a -> Bool) -> List a -> List a
+filter predicate list = []
+
+-- Change function body.
+sum : List Int -> Int
+sum list = 0
+
+-- Change function body.
+concat : List a -> List a -> List a
+concat list1 list2 = []
