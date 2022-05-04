2cd3da6931e64e1a3e511f3d733aa13ed9ec8ad5
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-05-04 10:48:06 +0200

    Add notes

--- src/Lists.elm
+++ src/Lists.elm
@@ -3,29 +3,65 @@ module Lists exposing (..)
 -- Compute the length of a List without using List.length.
 -- Change function body.
 length : List a -> Int
-length list = 0
+length li =
+  case li of
+    [] -> 0
+    hd :: tl -> 1 + (length tl)
 
 -- Checks if a value is includes in the List without using List.member.
 -- Change function body.
-member : a -> List a -> Bool
-member mem list = False
+member : Int -> List Int -> Bool
+member num li =
+  case li of
+    [] -> False
+    hd :: tl -> num == hd || member num tl
 
 -- Removes all elements at an even position in the List.
 -- Change function body.
-filterEven : List Int -> List Int
-filterEven list = []
+filterEven : List a -> List a
+filterEven li =
+  let
+    filter li =
+      case li of
+        [] -> []
+        (index, elem) ->
+          if index |> modBy 2 == 0 then
+            filter elem
+          else
+            elem :: filter elem
+  in
+    li
+    |> List.indexedMap Tuple.pair
+    |> filter
 
 -- Removes all elements not satistfying the predicate.
 -- Change function body.
-filter : (a -> Bool) -> List a -> List a
-filter predicate list = []
+filter : (a -> Bool) -> List a -> List b
+filter fun =
+  let
+    predicate elem =
+      case elem of
+        True -> [ elem ]
+        False -> []
+  in
+    List.concatMap predicate
 
 -- Computes the sum of a list of Int.
 -- Change function body.
 sum : List Int -> Int
-sum list = 0
+sum list = List.foldl (+) 0
+
+recSum : List Int -> Int
+recSum =
+  let
+    sumHelp acc li =
+      case li of
+        [] -> acc
+        hd :: tl -> sumHelp (acc + hd) tl
+  in
+    sumHelp 0
 
 -- Append the second list to the first without using List.append.
 -- Change function body.
 append : List a -> List a -> List a
-append list1 list2 = []
+append li1 li2 = List.foldr (::) li1 li2
--- src/Maths.elm
+++ src/Maths.elm
@@ -1,21 +1,79 @@
 module Maths exposing (..)
 
+{-| Exemples de récursion terminale et non-terminale.
+La récursion terminale permet d'éviter les changements de contexte,
+et évite d'accumuler de la mémoire sur la pile.
+Il faut réfléchir en terme d'accumulateurs et identifier les calculs
+nécessaires aux résultats pour les déplacer dans les accumulateurs. -}
+
 -- Add all the number from 1 to n.
 -- Change function body.
 sum : Int -> Int
-sum n = 0
+sum range =
+  if range == 0 then
+    range
+  else
+    range + sum (range - 1)
+
+-- Récursion terminale pour sum.
+termSum : Int -> Int -> Int
+termSum acc range =
+  if range == 0 then
+    acc
+  else
+    let newAcc = acc + range
+        newRange = range - 1 in
+    termSum newAcc newRange
+
+-- Helper pour s'assurer que la fonction sum est appelé correctement.
+sum : Int -> Int
+sum range = termSum 0 range
 
 -- Multiply all the numbers from 1 to n.
 -- Change function body.
 fact : Int -> Int
-fact n = 0
+fact n =
+  if n == 1 then
+    n
+  else n * (fact (n - 1))
+
+factTail : Int -> Int
+factTail =
+  let
+    factTailHelp : Int -> Int -> Int
+    factTailHelp acc n =
+      if n == 0 then
+        acc
+      else
+        factTailHelp (acc * n) (n - 1)
+  in
+    factTailHelp 0
 
 -- Compute the nth Fibonacci number.
 -- Change function body.
-fib : Int -> Int
-fib n = 0
+-- Récursion non-terminale pour fibonacci.
+fibonacci : Int -> Int
+fibonacci n =
+  if n == 0 || n == 1 then
+    n
+  else
+    let prev = fibonacci (n - 1)
+        otherPrev = fibonacci (n - 2) in
+    prev + otherPrev
+
+-- Récursion terminale pour Fibonacci.
+termFibonacci : Int -> Int -> Int -> Int
+termFibonacci acc1 acc2 n =
+  if n == 1 then
+    acc2
+  else
+    termFibonacci acc2 (acc1 + acc2) (n - 1)
 
 -- Compute the nth Fibonacci number in a tail-rec way.
 -- Change function body.
 fibTail : Int -> Int
-fibTail n = 0
+fibTail n =
+  if n == 0 || n == 1 then
+    n
+  else
+    termFibonacci 0 1 n
